f9fbde98ec752c46fb5ce0e51311b8e7b2815ad1
Weinan Qiu <davidiamyou@gmail.com>
2020-12-15 18:59:28 +0800

    add discovery

--- oidc/discovery.go
+++ oidc/discovery.go
@@ -1,5 +1,22 @@
 package oidc
 
+import (
+	"encoding/json"
+	"github.com/absurdlab/tiga-go-sdk/internal"
+	"io"
+	"time"
+)
+
+// ReadDiscovery reads a Discovery from io.Reader.
+func ReadDiscovery(reader io.Reader) (*Discovery, error) {
+	var d Discovery
+	if err := json.NewDecoder(reader).Decode(&d); err != nil {
+		return nil, err
+	}
+	return &d, nil
+}
+
+// Discovery is the specification defined OpenID Connect configuration metadata plus custom extensions
 type Discovery struct {
 	Issuer                                     string   `json:"issuer"`
 	AuthorizationEndpoint                      string   `json:"authorization_endpoint"`
@@ -97,5 +114,171 @@ func (d *Discovery) ResponseModesSupportedOrDefault() []string {
 	if len(d.ResponseModesSupported) > 0 {
 		return d.ResponseModesSupported
 	}
-	return []string{"todo"}
+	return []string{ResponseModeQuery, ResponseModeFragment}
+}
+
+// GrantTypesSupportedOrDefault returns Discovery#GrantTypesSupported if it is not
+// empty, or ["authorization_code", "implicit"] if it is empty.
+//
+//	grant_types_supported:
+// 	Dynamic OpenID Providers MUST support the authorization_code and implicit Grant Type
+//	values and MAY support other Grant Types. If omitted, the default value is
+//	["authorization_code", "implicit"].
+//
+// https://openid.net/specs/openid-connect-discovery-1_0.html#ProviderConfig
+func (d *Discovery) GrantTypesSupportedOrDefault() []string {
+	if len(d.GrantTypesSupported) > 0 {
+		return d.GrantTypesSupported
+	}
+	return []string{GrantTypeAuthorizationCode, GrantTypeImplicit}
+}
+
+// TokenEndpointAuthMethodsSupportedOrDefault returns Discovery#TokenEndpointAuthMethodsSupported if it
+// is not empty, or ["client_secret_basic"] if it is empty.
+//
+//	token_endpoint_auth_methods_supported:
+//	If omitted, the default is client_secret_basic -- the HTTP Basic Authentication Scheme specified
+//	in Section 2.3.1 of OAuth 2.0 [RFC6749].
+//
+// https://openid.net/specs/openid-connect-discovery-1_0.html#ProviderConfig
+func (d *Discovery) TokenEndpointAuthMethodsSupportedOrDefault() []string {
+	if len(d.TokenEndpointAuthMethodsSupported) > 0 {
+		return d.TokenEndpointAuthMethodsSupported
+	}
+	return []string{ClientSecretBasic}
+}
+
+// ClaimTypesSupportedOrDefault returns Discovery#ClaimTypesSupported if it is not empty, or ["normal"] if it is empty.
+//
+//	claim_types_supported:
+// 	If omitted, the implementation supports only normal Claims.
+//
+// https://openid.net/specs/openid-connect-discovery-1_0.html#ProviderConfig
+func (d *Discovery) ClaimTypesSupportedOrDefault() []string {
+	if len(d.ClaimTypesSupported) > 0 {
+		return d.ClaimTypesSupported
+	}
+	return []string{ClaimTypeNormal}
+}
+
+// ClaimsParameterSupportedOrDefault returns Discovery#ClaimsParameterSupported or false.
+//
+//	claims_parameter_supported:
+//	If omitted, the default value is false.
+//
+// https://openid.net/specs/openid-connect-discovery-1_0.html#ProviderConfig
+func (d *Discovery) ClaimsParameterSupportedOrDefault() bool {
+	if d.ClaimsParameterSupported != nil {
+		return *d.ClaimsParameterSupported
+	}
+	return false
+}
+
+// RequestParameterSupportedOrDefault returns Discovery#RequestParameterSupported or false
+//
+// 	request_parameter_supported:
+//	If omitted, the default value is false.
+//
+// https://openid.net/specs/openid-connect-discovery-1_0.html#ProviderConfig
+func (d *Discovery) RequestParameterSupportedOrDefault() bool {
+	if d.RequestParameterSupported != nil {
+		return *d.RequestParameterSupported
+	}
+	return false
+}
+
+// RequestURIParameterSupportedOrDefault returns Discovery#RequestURIParameterSupported or true
+//
+// 	request_uri_parameter_supported:
+//	If omitted, the default value is true.
+//
+// https://openid.net/specs/openid-connect-discovery-1_0.html#ProviderConfig
+func (d *Discovery) RequestURIParameterSupportedOrDefault() bool {
+	if d.RequestURIParameterSupported != nil {
+		return *d.RequestURIParameterSupported
+	}
+	return true
+}
+
+// RequireRequestURIRegistrationOrDefault returns Discovery#RequireRequestURIRegistration or false
+//
+// 	require_request_uri_registration:
+//	If omitted, the default value is false.
+//
+// https://openid.net/specs/openid-connect-discovery-1_0.html#ProviderConfig
+func (d *Discovery) RequireRequestURIRegistrationOrDefault() bool {
+	if d.RequireRequestURIRegistration != nil {
+		return *d.RequireRequestURIRegistration
+	}
+	return false
+}
+
+// CodeLifespanDuration returns the time.Duration of CodeLifespan.
+func (d *Discovery) CodeLifespanDuration() time.Duration {
+	return time.Duration(d.CodeLifespan) * time.Second
+}
+
+// AccessTokenLifespanDuration returns the time.Duration of AccessTokenLifespan
+func (d *Discovery) AccessTokenLifespanDuration() time.Duration {
+	return time.Duration(d.AccessTokenLifespan) * time.Second
+}
+
+// RefreshTokenLifespanDuration returns the time.Duration of RefreshTokenLifespan.
+func (d *Discovery) RefreshTokenLifespanDuration() time.Duration {
+	return time.Duration(d.RefreshTokenLifespan) * time.Second
+}
+
+// IdTokenLifespanDuration returns the time.Duration of IdTokenLifespan.
+func (d *Discovery) IdTokenLifespanDuration() time.Duration {
+	return time.Duration(d.IdTokenLifespan) * time.Second
+}
+
+// Clone returns a new Discovery object with the same parameters.
+func (d *Discovery) Clone() *Discovery {
+	return &Discovery{
+		Issuer:                                     d.Issuer,
+		AuthorizationEndpoint:                      d.AuthorizationEndpoint,
+		TokenEndpoint:                              d.TokenEndpoint,
+		UserInfoEndpoint:                           d.UserInfoEndpoint,
+		JSONWebKeySetURI:                           d.JSONWebKeySetURI,
+		RegistrationEndpoint:                       d.RegistrationEndpoint,
+		ScopesSupported:                            internal.CopyArray(d.ScopesSupported),
+		ResponseTypesSupported:                     internal.CopyArray(d.ResponseTypesSupported),
+		ResponseModesSupported:                     internal.CopyArray(d.ResponseModesSupported),
+		GrantTypesSupported:                        internal.CopyArray(d.GrantTypesSupported),
+		AcrValuesSupported:                         internal.CopyArray(d.AcrValuesSupported),
+		SubjectTypesSupported:                      internal.CopyArray(d.SubjectTypesSupported),
+		IdTokenSigningAlgValuesSupported:           internal.CopyArray(d.IdTokenSigningAlgValuesSupported),
+		IdTokenEncryptionAlgValuesSupported:        internal.CopyArray(d.IdTokenEncryptionAlgValuesSupported),
+		IdTokenEncryptionEncValuesSupported:        internal.CopyArray(d.IdTokenEncryptionEncValuesSupported),
+		UserInfoSigningAlgValuesSupported:          internal.CopyArray(d.UserInfoSigningAlgValuesSupported),
+		UserInfoEncryptionAlgValuesSupported:       internal.CopyArray(d.UserInfoEncryptionAlgValuesSupported),
+		UserInfoEncryptionEncValuesSupported:       internal.CopyArray(d.UserInfoEncryptionEncValuesSupported),
+		RequestObjectSigningAlgValuesSupported:     internal.CopyArray(d.RequestObjectSigningAlgValuesSupported),
+		RequestObjectEncryptionAlgValuesSupported:  internal.CopyArray(d.RequestObjectEncryptionAlgValuesSupported),
+		RequestObjectEncryptionEncValuesSupported:  internal.CopyArray(d.RequestObjectEncryptionEncValuesSupported),
+		TokenEndpointAuthMethodsSupported:          internal.CopyArray(d.TokenEndpointAuthMethodsSupported),
+		TokenEndpointAuthSigningAlgValuesSupported: internal.CopyArray(d.TokenEndpointAuthSigningAlgValuesSupported),
+		DisplayValuesSupported:                     internal.CopyArray(d.DisplayValuesSupported),
+		ClaimTypesSupported:                        internal.CopyArray(d.ClaimTypesSupported),
+		ClaimsSupported:                            internal.CopyArray(d.ClaimsSupported),
+		ServiceDocumentation:                       d.ServiceDocumentation,
+		UILocalesSupported:                         internal.CopyArray(d.UILocalesSupported),
+		ClaimsParameterSupported:                   internal.CopyBool(d.ClaimsParameterSupported),
+		RequestParameterSupported:                  internal.CopyBool(d.RequestParameterSupported),
+		RequestURIParameterSupported:               internal.CopyBool(d.RequestURIParameterSupported),
+		RequireRequestURIRegistration:              internal.CopyBool(d.RequireRequestURIRegistration),
+		OPPolicyURI:                                d.OPPolicyURI,
+		OPTermsOfServiceURI:                        d.OPTermsOfServiceURI,
+		AuthorizeResumeEndpoint:                    d.AuthorizeResumeEndpoint,
+		LoginEndpoint:                              d.LoginEndpoint,
+		SelectAccountEndpoint:                      d.SelectAccountEndpoint,
+		ConsentEndpoint:                            d.ConsentEndpoint,
+		CodeLifespan:                               d.CodeLifespan,
+		AccessTokenLifespan:                        d.AccessTokenLifespan,
+		RefreshTokenLifespan:                       d.RefreshTokenLifespan,
+		IdTokenLifespan:                            d.IdTokenLifespan,
+		CodeSigningAlgValue:                        d.CodeSigningAlgValue,
+		AccessTokenSigningAlgValue:                 d.AccessTokenSigningAlgValue,
+	}
 }
